9be24b8a4ae3263ce0a819b38a165e9a796cb517
HitendraNaik <naikhitendrae@gmail.com>
2023-11-14 20:07:32 +0530

    Done till 04exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedString=str.split('').reverse().join('');
+    return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+function removeFromArray(array,...element) {
+    element;
+    temp=array;
+    let position;
+    for(let i=0;i<element.length;i++){
+        if(temp.includes(element[i])){
+            position=temp.indexOf(element[i]);
+            temp.splice(position,1);
+        }
+    }
+    return temp;
 };
 
 // Do not edit below this line
