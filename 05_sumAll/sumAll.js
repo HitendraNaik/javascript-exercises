12dbab63a1d611b42b38b03aea8027d7fe4160a8
HitendraNaik <naikhitendrae@gmail.com>
2023-11-19 10:51:39 +0530

    Completed Exercise 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstTerm, lastTerm) {
+    
+    if(firstTerm<0 || lastTerm<0){
+        return "ERROR";
+    }else if(firstTerm>lastTerm){
+        let temp=firstTerm;
+        firstTerm=lastTerm;
+        lastTerm=temp;
+        let numberOfTerms = (lastTerm-firstTerm)+1;
+        let sum = (numberOfTerms/2)*(2*firstTerm+(numberOfTerms-1));
+        return sum;
+    }else if(typeof firstTerm==="number" && typeof lastTerm==="number"){
+        let numberOfTerms = (lastTerm-firstTerm)+1;
+        let sum = (numberOfTerms/2)*(2*firstTerm+(numberOfTerms-1));
+        return sum;
+    }else{
+        return "ERROR";
+    }
 };
-
+sumAll(12,29);
 // Do not edit below this line
 module.exports = sumAll;
